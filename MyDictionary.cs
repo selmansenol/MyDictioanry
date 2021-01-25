97cd86bd62db9617255d79416417376b711e2242
Selman <69358460+selmansenol@users.noreply.github.com>
2021-01-25 20:57:45 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        
+        TKey[] keyArray;
+        TValue[] valueArray;
+        
+        TKey[] tempKeyArray;
+        TValue[] tempValueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new TKey[0];
+            valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+
+            
+            keyArray = new TKey[keyArray.Length + 1];
+            valueArray = new TValue[valueArray.Length + 1];
+
+            
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+            }
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                valueArray[i] = tempValueArray[i];
+            }
+
+            
+            keyArray[keyArray.Length - 1] = key;
+            valueArray[valueArray.Length - 1] = value;
+        }
+
+        
+        public TKey[] Items1
+        {
+            get { return keyArray; }
+        }
+
+        public TValue[] Items2
+        {
+            get { return valueArray; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> persons = new MyDictionary<string, int>();
+            persons.Add("Selman Şenol", 22);
+            persons.Add("Ayça Temur", 20);
+            
+            Console.WriteLine("Ad Soyad: " + persons.Items1[0] + " " + "Yaş: " + persons.Items2[0]);
+            Console.WriteLine("Ad Soyad: " + persons.Items1[1] + " " + "Yaş: " + persons.Items2[1]);
+            
+        }
+    }
+}
